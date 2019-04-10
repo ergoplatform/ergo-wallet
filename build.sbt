ced6728533e481985a9cda67a041b0a1ff74a974
Ilya Oskin <ilya.arcadich@gmail.com>
2019-04-10 12:52:45 +0300

    Bip39 implementation.

--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+resolvers += "Sonatype Public" at "https://oss.sonatype.org/content/groups/public/"
+
 val sigmaStateVersion = "master-2b4b07a1-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  "org.scorexfoundation" %% "sigma-state" % sigmaStateVersion
+  "org.scorexfoundation" %% "sigma-state" % sigmaStateVersion,
+  "org.scodec" %% "scodec-bits" % "1.1.6",
+
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.14.+" % "test",
 )
---
+++ src/main/scala/org/ergoplatform/wallet/Mnemonic.scala
@@ -0,0 +1,70 @@
+package org.ergoplatform.wallet
+
+import java.text.Normalizer.Form.NFKD
+import java.text.Normalizer.normalize
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+import scodec.bits.BitVector
+
+import scala.util.{Failure, Try}
+
+class Mnemonic(languageId: String, strength: Int) {
+
+  import Mnemonic._
+
+  /**
+    * Generates new mnemonic phrase from system randomness.
+    */
+  def generate: Try[String] = {
+    if (!Mnemonic.AllowedStrengths.contains(strength))
+      Failure(new Error(s"Strength should be one of the following [128, 160, 192, 224, 256], but it is $strength."))
+    else toMnemonic(scorex.utils.Random.randomBytes(strength / 8))
+  }
+
+  /**
+    * Converts mnemonic phrase to seed it was derived from.
+    */
+  def toSeed(mnemonic: String, passphrase: Option[String] = None): Array[Byte] = {
+    val normalizedMnemonic = normalize(mnemonic.toCharArray, NFKD).toCharArray
+    val normalizedSeed = normalize(s"mnemonic${passphrase.getOrElse("")}", NFKD)
+
+    val spec = new PBEKeySpec(
+      normalizedMnemonic,
+      normalizedSeed.getBytes,
+      Pbkdf2Iterations,
+      Pbkdf2KeyLength
+    )
+    val skf = SecretKeyFactory.getInstance(Pbkdf2Algorithm)
+    skf.generateSecret(spec).getEncoded
+  }
+
+  /**
+    * Generates new mnemonic phrase from system randomness.
+    */
+  def toMnemonic(entropy: Array[Byte]): Try[String] = {
+    if (!Mnemonic.AllowedEntropyLengths.contains(entropy.length))
+      Failure(new Error(s"Entropy length should be one of the following: [16, 20, 24, 28, 32], but it is $strength."))
+    else {
+      val checksum = BitVector(scorex.crypto.hash.Sha256.hash(entropy))
+      val entropyWithChecksum = BitVector(entropy) ++ checksum.take(entropy.length / 4)
+
+      WordList.load(languageId).map { wordList =>
+        entropyWithChecksum.grouped(Mnemonic.BitsGroupSize).map { wordIndex =>
+          wordList.words(wordIndex.toInt(signed = false))
+        } mkString wordList.delimiter
+      }
+    }
+  }
+
+}
+
+object Mnemonic {
+  val MnemonicSentenceSizes: Seq[Int] = Seq(12, 15, 18, 21, 24)
+  val AllowedStrengths: Seq[Int] = Seq(128, 160, 192, 224, 256)
+  val AllowedEntropyLengths: Seq[Int] = Seq(16, 20, 24, 28, 32)
+  val BitsGroupSize = 11
+  val Pbkdf2Algorithm = "PBKDF2WithHmacSHA512"
+  val Pbkdf2Iterations = 2048
+  val Pbkdf2KeyLength = 512
+}
