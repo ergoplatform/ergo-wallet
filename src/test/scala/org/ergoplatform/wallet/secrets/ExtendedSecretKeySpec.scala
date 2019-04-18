e47a0986daf46e37534cad3e47b25bc97d045fbd
oskin1 <ilya.arcadich@gmail.com>
2019-04-18 16:16:32 +0300

    Hard index derivation check.

--- src/test/scala/org/ergoplatform/wallet/secrets/ExtendedSecretKeySpec.scala
+++ src/test/scala/org/ergoplatform/wallet/secrets/ExtendedSecretKeySpec.scala
@@ -9,16 +9,19 @@ class ExtendedSecretKeySpec extends PropSpec with Matchers {
     val expectedRoot = "4rEDKLd17LX4xNR8ss4ithdqFRc3iFnTiTtQbanWJbCT"
     val expectedKey11 = "CLdMMHxNtiPzDnWrVuZQr22VyUx8deUG7vMqMNW7as7M"
     val expectedKey22 = "9icjp3TuTpRaTn6JK6AHw2nVJQaUnwmkXVdBdQSS98xD"
+    val expectedKey3Hard2 = "DWMp3L9JZiywxSb5gSjc5dYxPwEZ6KkmasNiHD6VRcpJ"
     val seedStr = "5KgzUvF4yZjBDkoseNyZnAHsA6cuqhvwkGUNZ4y2WQXfRhLoHfbipZR4XriVZKbVFMcP6QKLRwLZhJdRt2wKx6tY"
     val seed = Base58.decode(seedStr).get
 
     val root = ExtendedSecretKey.fromSeed(seed)
     val k11 = root.child(1)
     val k22 = k11.child(2)
+    val k3h2 = k22.child(Index.hardIndex(2))
 
     Base58.encode(root.keyBytes) shouldEqual expectedRoot
     Base58.encode(k11.keyBytes) shouldEqual expectedKey11
     Base58.encode(k22.keyBytes) shouldEqual expectedKey22
+    Base58.encode(k3h2.keyBytes) shouldEqual expectedKey3Hard2
   }
 
 }
